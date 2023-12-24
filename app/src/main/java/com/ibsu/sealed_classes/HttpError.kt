8674766b7187b7a284a44838579633577a6fad29
tengiz <tengiz152@gmail.com>
2023-12-25 02:40:56 +0400

    important remark

--- app/src/main/java/com/ibsu/sealed_classes/HttpError.kt
+++ app/src/main/java/com/ibsu/sealed_classes/HttpError.kt
@@ -3,7 +3,7 @@ package com.ibsu.sealed_classes
 
 //Declaring child classes inside the sealed parent class (or in the same file) is a common/good practice
 //but not mandatory
-
+//No other subclasses may appear outside the module and package within which the sealed class is defined.
 sealed class HttpError(val code: Int) {
     class RedirectResponseException(code: Int) : HttpError(code)
     sealed class ClientRequestException(code: Int) : HttpError(code){
